b016bf5517bea687a30d88e00e1bf392c7f8478d
Aad Versteden <madnificent@gmail.com>
2020-06-09 22:33:59 +0200

    Removing dead code and formatting code
    Some of the code was left over dead as we introduced the state
    machine.

--- app/controllers/poi/new.ts
+++ app/controllers/poi/new.ts
@@ -1,6 +1,5 @@
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import Controller from '@ember/controller';
 import Store from '@ember-data/store';
 import { statechart } from 'ember-statecharts/computed'
@@ -57,21 +56,21 @@ export default class PoiNew extends Controller {
                 .store
                 .createRecord("point-of-interest", { label })
                 .save();
-            context.statechart.send("SAVED", { record } )
+            context.statechart.send("SAVED", { record })
           } catch (e) {
             context.statechart.send("FAIL");
           }
         },
-        showEntity( context: PoiNew, { record } : { record: PointOfInterest } ){
+        showEntity(context: PoiNew, { record }: { record: PointOfInterest }) {
           try {
             context.transitionToRoute('poi.show', record);
-          } catch( e ){
+          } catch (e) {
             context.statechart.send("FAIL");
           }
         }
       },
       guards: {
-        hasLabel(context : PoiNew){
+        hasLabel(context: PoiNew) {
           return context.label;
         }
       }
@@ -79,45 +78,6 @@ export default class PoiNew extends Controller {
   ) statechart!: Statechart;
 
   @service store!: Store;
-
-
-
-
-
-  /**
-   * Handles the submit event from the cursor.
-   */
-  @action
-  submit(event: Event) {
-    event.preventDefault();
-
-    if (this.label) {
-      this.createPoi(this.label as unknown as string);
-      this.reset();
-    } else {
-      this.state = "error"
-    }
-  }
-
-  /**
-   * Resets the controller.
-   */
-  reset() {
-    this.label = null;
-  }
-
-  /**
-   * Creates a new point-of-interest and transitions to it.
-   */
-  async createPoi(label: string) {
-    const record =
-      await this
-        .store
-        .createRecord("point-of-interest", { label })
-        .save();
-
-    this.transitionToRoute('poi.show', record);
-  }
 }
 
 // DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
--- app/routes/poi/new.ts
+++ app/routes/poi/new.ts
@@ -1,8 +1,3 @@
 import Route from '@ember/routing/route';
-import PoiNewController from 'frontend-toevla-data-entry/controllers/poi/new';
 
-export default class PoiNewRoute extends Route {
-  setupController( controller: PoiNewController ) {
-    controller.reset();
-  }
-}
+export default class PoiNewRoute extends Route { }
