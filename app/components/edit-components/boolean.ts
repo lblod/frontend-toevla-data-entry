832e805e4dee9ddd70d4a83fa48bff9c134886e5
Aad Versteden <madnificent@gmail.com>
2020-06-24 20:34:45 +0200

    Introduce statechart into boolean toggle

--- app/components/edit-components/boolean.ts
+++ app/components/edit-components/boolean.ts
@@ -1,10 +1,11 @@
-import { action } from '@ember/object';
 import { get } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 import TreeNode from 'frontend-toevla-data-entry/models/tree-node';
 import Experience from 'frontend-toevla-data-entry/models/experience';
 import { property, getInstance, setInstanceValue, save } from 'frontend-toevla-data-entry/utils/path-instances';
+import Statechart from 'ember-statecharts/utils/statechart';
+import { handler, guard, statechart } from 'frontend-toevla-data-entry/utils/rockin-statechart';
 
 interface EditComponentsBooleanArgs {
   key: string;
@@ -13,11 +14,40 @@ interface EditComponentsBooleanArgs {
 }
 
 export default class EditComponentsBoolean extends Component<EditComponentsBooleanArgs> {
-  @tracked currentInstance;
-  @tracked currentProperty: string;
-
+  @tracked currentInstance: any;
   @tracked hasSetValue: boolean = false;
   @tracked configuredValue: any = undefined;
+
+  @statechart(
+    {
+      initial: "init",
+      states: {
+        init: {
+          entry: ["initIntermediateObjects"],
+          on: {
+            SETUP_OBJECTS: "dataEntry"
+          }
+        },
+        dataEntry: {
+          on: {
+            SAVE: "saving"
+          }
+        },
+        saving: {
+          entry: ["save"],
+          on: {
+            SAVED: "dataEntry"
+          }
+        }
+      }
+    }
+  )
+  statechart!: Statechart;
+
+  get currentProperty(){
+    return property( this.args.key );
+  }
+
   get currentValue() {
     if (this.hasSetValue) {
       return this.configuredValue;
@@ -32,26 +62,22 @@ export default class EditComponentsBoolean extends Component<EditComponentsBoole
     this.hasSetValue = true;
   }
 
-  constructor() {
-    super(...arguments);
-    this.currentProperty = property(this.args.key);
-    this.initIntermediateObjects();
-  }
-
   /**
    * Ensures we have an intermediate object and a current state.
    */
+  @handler()
   async initIntermediateObjects() {
     this.currentInstance = await getInstance(this.args.experience, this.args.key);
+    this.statechart.send("SETUP_OBJECTS");
   }
 
-  @action
-  async save(event: Event){
-    event.preventDefault();
-
+  // @action
+  @handler()
+  async save(){
     if( this.hasSetValue ) {
       await setInstanceValue( this.args.experience, this.args.key, this.configuredValue );
       await save( this.args.experience, this.args.key );
     }
+    this.statechart.send("SAVED");
   }
 }
