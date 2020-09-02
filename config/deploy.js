960fbffbc494af285e5dd9c15ce68256945071aa
Aad Versteden <madnificent@gmail.com>
2020-09-02 16:57:29 +0200

    Adding deployment configuration to the development environment
    The development environment of the data entry stack can now be
    deployed more easily.

---
+++ config/deploy.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(deployTarget) {
+  //see https://github.com/ember-cli-deploy/ember-cli-deploy-revision-data/issues/52
+  process.env.GIT_DISCOVERY_ACROSS_FILESYSTEM=1;
+  let ENV = {
+    build: {
+      environment: 'production'
+    },
+    'ssh-index': { // copy and deploy index.html
+      username: 'root',
+      host: 'toevla-dev.s.redpencil.io',
+      port: 22,
+      remoteDir: '/data/app-toevla-dev/config/frontend-toevla-data-entry/',
+      allowOverwrite: true,
+      agent: process.env.SSH_AUTH_SOCK
+    },
+    'rsync': { // copy assets
+      host: 'root@toevla-dev.s.redpencil.io',
+      port: 22,
+      dest: '/data/app-toevla-dev/config/frontend-toevla-data-entry/',
+      delete: false,
+      arg:['--verbose']
+    }
+  };
+
+  if (deployTarget === 'development') {
+    // TODO
+  }
+
+  if (deployTarget === 'staging') {
+    // TODO
+  }
+
+  if (deployTarget === 'production') {
+    // TODO
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
