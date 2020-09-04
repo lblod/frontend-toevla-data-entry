8506ccba405831ad391e8c1c3f34f173e627e3be
Sergey Andreev <sergey.andreev@redpencil.io>
2020-09-04 12:46:11 +0200

    removed irrelevant files

--- app/routes/file-test.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class FileTestRoute extends Route {
-  async model(){
-
-    const poi=await this.store.findAll('point-of-interest');
-    return poi.firstObject;
-  }
-}
