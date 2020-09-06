bda351d49bbd31fa263d41479a7efcbd565dd89a
Aad Versteden <madnificent@gmail.com>
2020-09-06 20:44:34 +0200

    More lenient target environments

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
+  'last 4 Chrome versions',
+  'last 4 Firefox versions',
+  'last 2 Safari versions'
 ];
 
 const isCI = !!process.env.CI;
