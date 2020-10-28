1ee655bd40282618f824c406d0b9f2d9ddb34725
Aad Versteden <madnificent@gmail.com>
2020-10-28 01:25:06 +0100

    Always render the main experience in the tree
    This not-so-clear code allows us to render the desired experience in
    the tree.  It should suffice until we have a sturdy backend solution.

--- app/routes/experience/show/tree.ts
+++ app/routes/experience/show/tree.ts
@@ -17,8 +17,25 @@ export default class ExperienceShowTree extends Route {
     return model.firstObject;
   }
 
+  async afterModel() {
+    const experience = this.modelFor("experience.show") as Experience;
+    /*
+     * NOTE: this is a hack to render the right experience in the
+     * tree, as expected.  You can also see this in this.setupController,
+     * in the Controller (as a tracked property), as well as in the
+     * template as the basis for the tree.  We should rewrite this code
+     * as we progress with inheritance.
+     */
+    this.mainExperience = (
+      await this.store.query( 'experience', {
+        "filter[point-of-interest][experiences][:id:]": experience.id,
+        "filter[is-main-experience]": true
+      })).firstObject;
+  }
+
   setupController( controller: ExperienceShowTreeController, model: Tree ) {
     controller.model = model;
     controller.experience = this.modelFor("experience.show") as Experience;
+    controller.mainExperience = this.mainExperience;
   }
 }
