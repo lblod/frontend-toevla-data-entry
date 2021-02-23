feac366f11aedbfc131bd315ff96f478537c148c
Aad Versteden <madnificent@gmail.com>
2021-02-23 07:54:22 +0100

    First draft of SmartStore
    SmartStore can be slow in saving items.  Some caveats in the current
    implementation which should be sorted out.

---
+++ app/services/smart-store.js
@@ -0,0 +1,146 @@
+import Service from '@ember/service';
+import { tracked } from 'tracked-built-ins';
+
+
+class SaveInfo {
+  // item to save
+  instance = null;
+  // information to transition on failed save
+  routeInfos = tracked([]);
+  // timestamp indicating last change
+  @tracked lastRequest = null;
+  // timestamp indicating creation
+  @tracked firstRequest = null;
+
+  // Creates a new SaveInfo item
+  constructor(instance, routeInfo) {
+    this.instance = instance;
+    this.routeInfos.push(routeInfo);
+    this.firstRequest = new Date();
+    this.lastRequest = this.firstRequest;
+  }
+
+  // Updates an existing SaveInfo item
+  update(routeInfo) {
+    this.routeInfos = [...this.routeInfos, routeInfo];
+    this.lastRequest = new Date();
+  }
+}
+
+export default class SmartStoreService extends Service {
+  // all waiting outstanding changes
+  // _waiting = tracked({}); // doesn't operate as we hoped
+  @tracked _waiting = {};
+  // all executing outstanding changes
+  _executing = tracked(Set);
+  // all failed changes
+  _failed = tracked([]);
+
+  // a timeout to save each outstanding change
+  timeouts = new WeakMap();
+
+  /**
+    * Yields an Array of all outstanding updates.  Handy for rendering an
+    * update icon.
+    */
+  get outstandingUpdates() {
+    return Object.keys(this._waiting)
+      .map((key) => this._waiting[key])
+      .sort((a, b) => a.lastRequest <= b.lastRequest);
+  }
+
+  /**
+     * Yields a tracked Set of changes which are currently being executed.
+     *
+     * These are the requests currently being sent to the backend.
+     */
+  get executingUpdates() {
+    return [...this._executing];
+  }
+
+  /**
+     * Yields a tracked Array of updates which could not be executed.
+     *
+     * These are requests which have failed to execute.
+     */
+  get failedUpdates() {
+    return this
+      ._failed
+      .slice()
+      .sort((a, b) => a.lastRequest <= b.lastRequest);
+  }
+
+  /**
+     * Persist an item, this is like calling .save on the item.
+     */
+  persist(item, routeInfo) {
+    // save new item directly
+    if (item.isNew)
+      item.save();
+    else
+      this._addOutstandingChange(item, routeInfo);
+  }
+
+  hasOutstandingUpdate(item) {
+    return this._waiting[item.id];
+  }
+
+  // Adds an outstanding change to the backlog
+  _addOutstandingChange(item, routeInfo) {
+    let saveInfo;
+    if (this.hasOutstandingUpdate(item)) {
+      saveInfo = this._waiting[item.id];
+      saveInfo.update(routeInfo);
+      clearTimeout(this.timeouts.get(saveInfo));
+    } else {
+      saveInfo = new SaveInfo(item, routeInfo);
+      this._waiting[item.id] = saveInfo;
+      this._waiting = { ...this._waiting };
+    }
+
+    this.timeouts.set(
+      saveInfo,
+      setTimeout(
+        () => this._executeItemSaveOnTimeout(saveInfo),
+        5000));
+  }
+
+  async _executeItemSaveOnTimeout(saveInfo) {
+    const item = saveInfo.instance;
+    this._removeTimeout(saveInfo);
+    this._moveToExecuting(saveInfo);
+    try {
+      await item.save();
+    } catch (e) {
+      this._moveToFailed(saveInfo);
+    } finally {
+      this._removeFromExecuting(saveInfo);
+    }
+  }
+
+  _removeTimeout(saveInfo) {
+    let tim = this.timeouts.get(saveInfo);
+    if (tim)
+      clearTimeout(tim);
+
+    this.timeouts.delete(saveInfo);
+  }
+
+  _moveToExecuting(saveInfo) {
+    const item = saveInfo.instance;
+    console.log(this._waiting);
+    delete this._waiting[item.id];
+    this._waiting = {...this._waiting};
+    console.log(this._waiting);
+    this._executing.add(saveInfo);
+  }
+
+  _removeFromExecuting(saveInfo) {
+    this._executing.delete(saveInfo);
+  }
+
+  _moveToFailed(saveInfo) {
+    this._executing.delete(saveInfo);
+    this._failed.push(saveInfo);
+  }
+}
