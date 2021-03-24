43f50ffc4198a85139a42e777f846edf4c5ef7ed
Aad Versteden <madnificent@gmail.com>
2021-03-24 13:49:19 +0100

    Very crude and basic image sorting support
    Some next steps following.  We can now sort the items and we can
    auto-save that state.

---
+++ app/components/poi-picture-manipulator.js
@@ -0,0 +1,40 @@
+import { inject as service } from '@ember/service';
+import { tracked } from 'tracked-built-ins';
+import { A } from '@ember/array';
+import { computed } from '@ember/object';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { Resource, use } from 'ember-could-get-used-to-this';
+
+class PictureListResource extends Resource {
+  arr = tracked([]);
+
+  get value() {
+    return this.arr;
+  }
+
+  setup() {
+    const poi = this.args.positional[0];
+    this.arr = tracked(
+      poi
+        .images
+        .toArray()
+        .sortBy( "order" ));
+  }
+}
+
+export default class PoiPictureManipulatorComponent extends Component {
+  @use images = new PictureListResource( () => [this.args.poi]);
+
+  @service smartStore
+
+  @action
+  onSort(stuff) {
+    const images = [...this.images];
+    for( let i = 0; i < images.length; i++ ) {
+      images[i].order = i;
+      this.smartStore.persist( images[i] );
+    }
+    console.log(`We sorted stuff!`, stuff);
+  }
+}
