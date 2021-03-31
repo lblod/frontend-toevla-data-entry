822c87d4e4fafc20d97acd6ef638c7698a1a433f
Aad Versteden <madnificent@gmail.com>
2021-03-31 22:37:49 +0200

    Our own (improved) implementation of ember-a11y-refocus
    Within our own scope, setting a query parameter shouldn't replace
    focus.  This is somewhat of a hack in which we've also Octanified the
    component.  Seems to make sense to remove the dependency too.

--- app/components/au-navigation-narrator.js
+++ app/components/au-navigation-narrator.js
@@ -1,5 +1,19 @@
-import NavigationNarrator from 'ember-a11y-refocus/components/navigation-narrator';
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import { schedule } from '@ember/runloop';
 
-export default NavigationNarrator.extend({
+export default class NavigationNarratorComponent extends Component {
+  @service router;
 
-});
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeDidChange', (transition) => {
+      if( transition.from?.name !== transition.to?.name ) {
+        schedule('afterRender', this, function() {
+          document.body.querySelector('#ember-a11y-refocus-nav-message').focus();
+        });
+      }
+    });
+  }
+}
