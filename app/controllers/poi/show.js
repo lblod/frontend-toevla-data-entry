a5bd756007ee6d042028e8ff751a3fbb87cffb71
Aad Versteden <madnificent@gmail.com>
2021-05-07 02:31:32 +0200

    Implementation of transfer museum buttons
    The museum can be transferred from the museum to inter, from inter to
    the museum and from inter to public.  The buttons have been implemented
    and they make the necessary call to the backend.  We currently assume
    the transfer will be executed within time.  If it turns out that cannot
    be achieved on the server, a different information mechanism should be
    used with tasks.

--- app/controllers/poi/show.js
+++ app/controllers/poi/show.js
@@ -1,6 +1,36 @@
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import fetch from 'fetch';
 
 export default class PoiShowController extends Controller {
   @service smartStore;
+  @service currentAccount;
+  @tracked publishModal;
+  @tracked sendModal;
+
+  @action
+  async publish() {
+    const id = this.model.id;
+    const resp = await fetch(`/transfer/museum/${id}/send-to-public`, { method: 'POST' });
+    const body = await resp.json();
+    this.publishModal = false;
+  }
+
+  @action
+  async sendToMuseum() {
+    const id = this.model.id;
+    const resp = await fetch(`/transfer/museum/${id}/send-to-museum`, { method: 'POST' });
+    const body = await resp.json();
+    this.sendModal = false;
+  }
+
+  @action
+  async sendToValidator() {
+    const id = this.model.id;
+    const resp = await fetch(`/transfer/museum/${id}/send-to-validator`, { method: 'POST' });
+    const body = await resp.json();
+    this.sendModal = false;
+  }
 }
