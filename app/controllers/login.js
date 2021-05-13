af0e1d69505e830a6b48551e3af02e860b710e99
Aad Versteden <madnificent@gmail.com>
2021-05-13 13:14:39 +0200

    Allow requesting of login key
    We can now request a login key.  This will then trigger the sending of
    an email to the user with a login with login link.

---
+++ app/controllers/login.js
@@ -0,0 +1,30 @@
+import { isEmpty } from '@ember/utils';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import Controller from '@ember/controller';
+import fetch from 'fetch';
+
+export default class LoginController extends Controller {
+  @tracked emailAddress;
+  @tracked requesting = false;
+  @tracked requested = false;
+
+  get disableInput() {
+    return this.requesting || this.requested;
+  }
+
+  @action
+  async requestLoginLink(address) {
+    if (isEmpty(address)) {
+      console.log("No address given");
+    } else {
+      this.requesting = true;
+      const req = await fetch(`/email-login/emails/${address}/keys`, {
+        method: "POST"
+      });
+      console.log( await req.json() );
+      this.requested = true;
+      this.requesting = false;
+    }
+  }
+}
