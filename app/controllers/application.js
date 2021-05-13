88b1e17dc377358e2860a7ba8229a8d07ebae081
Aad Versteden <madnificent@gmail.com>
2021-05-13 13:14:27 +0200

    Implement logout button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,13 @@
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
   @service session;
+
+  @action
+  logout(event) {
+    event.preventDefault();
+    this.session.invalidate();
+  }
 }
