0e2fd884ae8496b1f8f720ec67721561c5603f2a
Aad Versteden <madnificent@gmail.com>
2021-05-16 14:43:40 +0200

    Implement switch role button
    The switch role button should allow users to log in for various roles.
    For this, the user is returned to the switch role route in which they
    can select the role they want to act through.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,10 +4,17 @@ import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
   @service session;
+  @service router;
 
   @action
   logout(event) {
     event.preventDefault();
     this.session.invalidate();
+  }
+
+  @action
+  selectRole(event) {
+    event.preventDefault();
+    this.router.transitionTo('session.roles');
   }
 }
