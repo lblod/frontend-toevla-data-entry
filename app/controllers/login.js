5d8db9e72009618810cd3d602be4a4a354f15070
Aad Versteden <madnificent@gmail.com>
2021-05-16 17:35:37 +0200

    Set accept header on request email call
    Setting the accept header in the Fetch call will make it easier for the
    dispatcher to select the right backing microservice.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,7 +2,7 @@ import { isEmpty } from '@ember/utils';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import Controller from '@ember/controller';
-import fetch from 'fetch';
+import fetch, { Headers } from 'fetch';
 
 export default class LoginController extends Controller {
   @tracked emailAddress;
@@ -20,9 +20,12 @@ export default class LoginController extends Controller {
     } else {
       this.requesting = true;
       const req = await fetch(`/email-login/emails/${encodeURIComponent(address)}/keys`, {
-        method: "POST"
+        method: "POST",
+        headers: new Headers({
+          'Accept': "application/vnd.api+json"
+        })
       });
-      console.log( await req.json() );
+      await req.json();
       this.requested = true;
       this.requesting = false;
     }
