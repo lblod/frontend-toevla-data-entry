dc96481ef99ad3910f9ba071f24aa31e8d5579be
Aad Versteden <madnificent@gmail.com>
2021-05-26 14:38:48 +0200

    Render username if available
    If a user is logged in, we now render their username.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,33 @@
+import { get } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
+import { Resource, use } from  'ember-could-get-used-to-this';
+
+class UserNameResource extends Resource {
+  @tracked userName = "Username"
+
+  get value() {
+    return this.userName;
+  }
+
+  async setup() {
+    try {
+      const person = await this.args.positional[0];
+      this.userName = `${person.firstName} ${person.lastName}`;
+    } catch(e) {
+      // Nothing to do, keeping default username
+    }
+  }
+}
 
 export default class ApplicationController extends Controller {
   @service session;
   @service router;
+  @service currentAccount;
+
+  @use username = new UserNameResource(() => [this.currentAccount.account.person]);
 
   @action
   logout(event) {
