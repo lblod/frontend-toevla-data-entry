4b4421b6e1ab3be14394d843001ecbeb6e26b1b6
Aad Versteden <madnificent@gmail.com>
2021-05-29 13:26:21 +0200

    Use new role service for communication around roles

--- app/routes/session/roles.js
+++ app/routes/session/roles.js
@@ -1,5 +1,6 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import fetch, { Headers } from 'fetch';
 
 export default class SessionRolesRoute extends Route {
   @service('session') sessionService;
@@ -7,6 +8,15 @@ export default class SessionRolesRoute extends Route {
 
   beforeModel(transition) {
     return this.sessionService.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    return (await
+      (await fetch('/session/roles', {
+        headers: new Headers({
+          accept: "application/vnd.api+json"
+        })
+      })).json()).data;
   }
 
   afterModel() {
