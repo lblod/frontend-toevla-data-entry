c64df0b6006d07b47777ea667fe6f9059b84d61f
Aad Versteden <madnificent@gmail.com>
2022-08-27 10:02:16 +0200

    Add known widget URIs
    The widgets are sent to the frontend in one go.  This commit adds the
    currently known widget UUIDs which should allow us to render the link to
    the widget automatically in the future.

--- app/routes/buildings.js
+++ app/routes/buildings.js
@@ -29,55 +29,72 @@ export default class BuildingsRoute extends Route {
         widget: "5FABA33DEF5FF2000800011A"
       }, {
         title: "GUM - Gents Universiteitsmuseum",
-        url: "https://www.gum.gent/nl/toegankelijkheid"
+        url: "https://www.gum.gent/nl/toegankelijkheid",
+        widget: "5FB3626BEF5FF20008000015"
       }, {
         title: "Design Museum Gent",
-        url: "https://www.designmuseumgent.be/toegankelijkheid"
+        url: "https://www.designmuseumgent.be/toegankelijkheid",
+        widget: "5F9A82DFEF5FF2000800007D"
       }, {
         title: "Industriemuseum",
-        url: "https://www.industriemuseum.be/nl/toegankelijkheid"
+        url: "https://www.industriemuseum.be/nl/toegankelijkheid",
+        widget: "5FA3B298EF5FF2000800002D"
       }, {
         title: "Sint-Pietersabdij",
-        url: "https://historischehuizen.stad.gent/nl/sint-pietersabdij/toegankelijkheid"
+        url: "https://historischehuizen.stad.gent/nl/sint-pietersabdij/toegankelijkheid",
+        widget: "5FA3DEA2EF5FF2000800007B"
       }, {
         title: "Gravensteen",
-        url: "https://historischehuizen.stad.gent/nl/gravensteen/toegankelijkheid"
+        url: "https://historischehuizen.stad.gent/nl/gravensteen/toegankelijkheid",
+        widget: "5FA41C15EF5FF200080000B4"
       }, {
         title: "STAM - Stadsmuseum Gent",
-        url: "https://www.stamgent.be/nl_be/bezoek-het-stam/toegankelijkheid"
+        url: "https://www.stamgent.be/nl_be/bezoek-het-stam/toegankelijkheid",
+        widget: "5FB23B0CEF5FF20008000002"
       }, {
         title: "Damiaanmuseum",
-        url: "http://damiaanmuseum.be/toegankelijkheid/"
+        url: "http://damiaanmuseum.be/toegankelijkheid/",
+        widget: "610D05F55E73B50008000090"
       }, {
         title: "Museum Voor Religieuze Kunst Diest",
-        url: "https://diest.be/museum-voor-religieuze-kunst"
+        url: "https://diest.be/museum-voor-religieuze-kunst",
+        widget: "610D275C5E73B500080000DF"
       }, {
         title: "Bezoekerscentrum Dru!f, huis van de tafeldruif",
-        url: "https://www.overijse.be/druif-toegankelijkheid"
+        url: "https://www.overijse.be/druif-toegankelijkheid",
+        widget: "610D18EA5E73B500080000D5"
       }, {
         title: "Hidrodoe",
-        url: "https://www.hidrodoe.be/plan-je-dag/toegankelijkheid"
+        url: "https://www.hidrodoe.be/plan-je-dag/toegankelijkheid",
+        widget: "610D14945E73B500080000B0"
       }, {
         title: "Abdijmuseum Ten Duinen",
-        url: "https://www.tenduinen.be/nl/toegankelijkheid"
+        url: "https://www.tenduinen.be/nl/toegankelijkheid",
+        widget: "60B52839560CA50008000058"
       }, {
         title: "Museum Hof van Busleyden",
-        url: "https://www.hofvanbusleyden.be/toegankelijkheid"
+        url: "https://www.hofvanbusleyden.be/toegankelijkheid",
+        widget: "6148AB2E5E73B5000900012D"
       }, {
         title: "Museum aan de IJzer",
-        url: "https://www.museumaandeijzer.be/nl/plan-je-bezoek/#toegankelijkheid"
+        url: "https://www.museumaandeijzer.be/nl/plan-je-bezoek/#toegankelijkheid",
+        widget: "6148AACE5E73B5000900012B"
       }, {
         title: "Het James Ensorhuis",
-        url: "https://www.ensorstad.be/nl/toegankelijkheid"
+        url: "https://www.ensorstad.be/nl/toegankelijkheid",
+        widget: "60D47D535E73B5000800001E"
       }, {
         title: "Gallo-Romeins Museum",
-        url: "https://www.galloromeinsmuseum.be/nl/praktisch/toegankelijkheid"
+        url: "https://www.galloromeinsmuseum.be/nl/praktisch/toegankelijkheid",
+        widget: "610D14555E73B500080000AE"
       }, {
         title: "DIVA - Museum voor diamant, juwelen en zilver",
-        url: "https://www.divaantwerp.be/nl/visit/toegankelijkheid"
+        url: "https://www.divaantwerp.be/nl/visit/toegankelijkheid",
+        widget: "610D07EB5E73B5000800009A"
       }, {
         title: "Museum44 Meensel-Kiezegem",
-        url: "https://museum44.be/toegankelijkheid/"
+        url: "https://museum44.be/toegankelijkheid/",
+        widget: "610D13FC5E73B500080000AA"
       }
     ];
   }
